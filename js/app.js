799a4ae9876b59dae768629ea35dfa149e40934d
vidyashevale24 <vidya.shevale24@gmail.com>
2019-08-01 15:14:06 +0530

    new files created

---
+++ js/app.js
@@ -0,0 +1,55 @@
+//Understanding array and functions
+/*var arr =[1, 'Vidya', 3 ,function(){
+    alert("hi");
+}];
+
+arr[3]();*/
+
+//Understanding dependancy injections and services
+
+/*var myApp = angular.module('myApp', ['ngMessages','ngResource']);
+myApp.controller('mainCtrl', function ($scope ,$log ,$filter ,$resource) {
+    
+    $scope.name = 'Vidya';
+    $scope.occupation = 'Software Developer';
+    $log.info($scope.name);
+
+    console.log($scope);
+    console.log($log);
+    $log.log("Hello Vidya");
+    $log.info("This is some information");
+    $log.warn("This is a warning message");
+    $log.debug("This is some debug code");
+    $log.error("It's an error message");
+    
+    console.log($resource);
+    $scope.name = 'Vidya';
+    $scope.formatedName = $filter('uppercase')($scope.name);
+    $log.info($scope.name);
+    $log.info($scope.formatedName);
+ });*/
+
+
+var myApp = angular.module('myApp', ['ngMessages','ngResource']);
+myApp.controller('mainCtrl', ['$scope' ,'$log', '$filter' ,'$resource' ,'$timeout' ,function( $scope ,$log, $filter ,$resource ,$timeout) {
+    
+    $scope.name = 'Vidya';
+    $scope.occupation = 'Software Developer';
+    
+    $timeout(function(){
+        $scope.name = 'Sagar';
+    },3000);
+    
+}]);
+
+//Compressed code of above 
+//myApp.controller("mainCtrl",["$scope","$log","$filter","$resource",function(a,c,d,b){a.name="Vidya";a.occupation="Software Developer";c.info(a.name)}]);
+
+
+//accessing array values
+/*
+var searchPeople = function ($scope,firstName , lastName , height, age , occupation ){
+    return 'Vidya Sagar';
+}
+
+console.log(angular.injector().annotate(searchPeople));*/

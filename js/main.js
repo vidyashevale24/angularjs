4014ef6b0656baeb1e3d81fa89f91ee2072d7fdf
vidyashevale24 <vidya.shevale24@gmail.com>
2019-08-03 13:27:22 +0530

    Serices implememtation

--- js/main.js
+++ js/main.js
@@ -14,14 +14,45 @@ myApp.config(['$routeProvider','$locationProvider', function($routeProvider,$loc
 		templateUrl:'pages/contact.html',
 		controller:'contactCtrl'
 	})
+	.when('/cont/:num',{	
+		templateUrl:'pages/contact.html',
+		controller:'contactCtrl'
+	})
 }]);
 
-myApp.controller('mainCtrl',['$scope',function($scope){
-	$scope.name = "Main";
+myApp.service('nameService',function(){
+	var self  =	 this;
+	this.name =	 'Vidya Sagar';
+	
+	this.nameLength = function(){
+		return self.name.length;
+	}
+});
+
+myApp.controller('mainCtrl',['$scope','$log','nameService',function($scope,$log,nameService){
+	/*$scope.name = "Main";
+	$log.main = "property from main";
+	$log.log($log);*/
+	$scope.name = nameService.name;
+	$scope.$watch('name',function(){
+		nameService.name = $scope.name;
+	});
+	
+	$log.log (nameService.name);
+	$log.log (nameService.nameLength());
 }]);
 
-myApp.controller('contactCtrl',['$scope',function($scope){
-	$scope.name = "Conatct";
+myApp.controller('contactCtrl',['$scope','$log','$routeParams','nameService',function($scope,$log,$routeParams,nameService){
+	$scope.num = $routeParams.num || 1;
+	$log.contact = "property from contact";
+	$log.log($log);
+	$log.log(nameService.name);
+	$log.log(nameService.nameLength());
+	
+	$scope.name = nameService.name;
+	$scope.$watch('name',function(){
+		nameService.name = $scope.name;
+	});
 }]);
 
 
